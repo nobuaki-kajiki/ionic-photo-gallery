e7280d53f9a4c40743d12e150fc8f10d67405541
Nobuaki.Kajiki <nobuaki.kaiki@avanade.com>
2019-12-26 17:06:52 +0900

    フォトサービス作成

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Camera,CameraOptions } from '@ionic-native/camera/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
 
-  constructor() { }
+  public photos:Photo[] = [];
+
+  constructor(
+    private camera:Camera,
+  ) { }
+
+  takePicture(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((imageData) =>{
+      this.photos.unshift({
+        data: 'data:image/JPEG;base64' + imageData
+      });
+    }, (err) =>{
+      console.log("Camera issue:" + err);
+    });
+  }
+}
+class Photo{
+  data:any;
 }
